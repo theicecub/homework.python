a2e08205ce4edd76d4468148558ec98ff8ff80d3
theicecub <banan8936@gmail.com>
2024-11-24 00:22:45 +0500

    homework ready

--- class.py
+++ class.py
@@ -1,1 +1,12 @@
 #1
+class Cat:
+    def __init__(self, start, middle, end):
+        self.start = start
+        self.middle = middle
+        self.end = end
+
+    def cat_life(self):
+     return f"Start of his life: {self.start} \n Middle of his life: {self.middle} \n End of his life: {self.end}"
+
+cat=Cat("He borns as a homeless", "He was find by some kind person", "He dies as a very good cat")
+print(cat.cat_life())
